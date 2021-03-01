3ddc2f18d9bc203f35924a65902aa1ca2bae41ca
Ramon <ramonbarbosa123456789@gmail.com>
2021-03-01 12:28:47 -0300

    Primeiro Commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <title>Plataformar de Estudo</title>
+</head>
+
+<body>
+    
+    <?php require('navbar.php');?>
+
+
+    <a href="login/" >Login</a>
+    <a>Registrar</a>
+
+    <?php require('footer.php');?>
+
+    <script src="js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
